d5ffa1f99f96fd55911d393ad07a775ec18f52a9
Shawn Tabrizi <shawntabrizi@gmail.com>
2019-01-09 16:49:37 +0100

    Trying to update to CC

--- substrate-cryptokitties/runtime/src/lib.rs
+++ substrate-cryptokitties/runtime/src/lib.rs
@@ -27,7 +27,7 @@ extern crate srml_executive as executive;
 extern crate srml_consensus as consensus;
 extern crate srml_timestamp as timestamp;
 extern crate srml_balances as balances;
-extern crate srml_upgrade_key as upgrade_key;
+extern crate srml_sudo as sudo;
 extern crate srml_aura as aura;
 extern crate substrate_consensus_aura_primitives as consensus_aura;
 
@@ -100,8 +100,8 @@ pub mod opaque {
 pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("substrate-cryptokitties"),
 	impl_name: create_runtime_str!("substrate-cryptokitties"),
-	authoring_version: 1,
-	spec_version: 1,
+	authoring_version: 2,
+	spec_version: 2,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 };
@@ -176,9 +176,10 @@ impl balances::Trait for Runtime {
 	type Event = Event;
 }
 
-impl upgrade_key::Trait for Runtime {
+impl sudo::Trait for Runtime {
 	/// The uniquitous event type.
 	type Event = Event;
+	type Proposal = Call;
 }
 
 impl cryptokitties::Trait for Runtime {
@@ -196,8 +197,8 @@ construct_runtime!(
 		Consensus: consensus::{Module, Call, Storage, Config<T>, Log(AuthoritiesChange), Inherent},
 		Aura: aura::{Module},
 		Balances: balances,
-		UpgradeKey: upgrade_key,
-		CryptoKitties: cryptokitties::{Module, Call, Storage},
+		Sudo: sudo,
+		Cryptokitties: cryptokitties::{Module, Call, Storage},
 	}
 );
 
@@ -212,7 +213,7 @@ pub type Block = generic::Block<Header, UncheckedExtrinsic>;
 /// BlockId type as expected by this runtime.
 pub type BlockId = generic::BlockId<Block>;
 /// Unchecked extrinsic type as expected by this runtime.
-pub type UncheckedExtrinsic = generic::UncheckedMortalExtrinsic<Address, Nonce, Call, Ed25519Signature>;
+pub type UncheckedExtrinsic = generic::UncheckedMortalCompactExtrinsic<Address, Nonce, Call, Ed25519Signature>;
 /// Extrinsic type that has already been checked.
 pub type CheckedExtrinsic = generic::CheckedExtrinsic<AccountId, Nonce, Call>;
 /// Executive: handles dispatch to the various modules.
