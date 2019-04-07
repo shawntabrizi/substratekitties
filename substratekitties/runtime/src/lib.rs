b4d2a3b0b32c4397fcd7f796389d94fbab1d7f54
Shawn Tabrizi <shawntabrizi@gmail.com>
2019-04-07 12:39:32 +0900

    Patch Substratekitties module for Substrate v1.0

--- substratekitties/runtime/src/lib.rs
+++ substratekitties/runtime/src/lib.rs
@@ -55,8 +55,7 @@ pub type BlockNumber = u64;
 /// Index of an account's extrinsic in the chain.
 pub type Nonce = u64;
 
-/// Used for the module template in `./template.rs`
-mod template;
+mod substratekitties;
 
 /// Opaque types. These are used by the CLI to instantiate machinery that don't need to know
 /// the specifics of the runtime. They can then be made to be agnostic over specific formats
@@ -187,8 +186,7 @@ impl sudo::Trait for Runtime {
 	type Proposal = Call;
 }
 
-/// Used for the module template in `./template.rs`
-impl template::Trait for Runtime {
+impl substratekitties::Trait for Runtime {
 	type Event = Event;
 }
 
@@ -205,8 +203,7 @@ construct_runtime!(
 		Indices: indices,
 		Balances: balances,
 		Sudo: sudo,
-		// Used for the module template in `./template.rs`
-		TemplateModule: template::{Module, Call, Storage, Event<T>},
+		Substratekitties: substratekitties::{Module, Call, Storage, Event<T>},
 	}
 );
 
