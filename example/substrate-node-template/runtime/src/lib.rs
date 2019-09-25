644d0157d82af118e98f5fcdef40ede5c5aba8a3
Shawn Tabrizi <shawntabrizi@gmail.com>
2019-09-25 04:13:05 +0200

    update deps

--- example/substrate-node-template/runtime/src/lib.rs
+++ example/substrate-node-template/runtime/src/lib.rs
@@ -61,9 +61,6 @@ pub type Hash = primitives::H256;
 /// Digest item type.
 pub type DigestItem = generic::DigestItem<Hash>;
 
-/// Used for the module template in `./template.rs`
-mod template;
-
 /// Opaque types. These are used by the CLI to instantiate machinery that don't need to know
 /// the specifics of the runtime. They can then be made to be agnostic over specific formats
 /// of data like extrinsics, allowing for them to continue syncing the network through upgrades
@@ -252,9 +249,10 @@ impl sudo::Trait for Runtime {
 	type Proposal = Call;
 }
 
-/// Used for the module template in `./template.rs`
-impl template::Trait for Runtime {
+impl substratekitties::Trait for Runtime {
 	type Event = Event;
+	type KittyIndex = u64;
+	type Currency = Balances;
 }
 
 construct_runtime!(
@@ -270,8 +268,7 @@ construct_runtime!(
 		Indices: indices::{default, Config<T>},
 		Balances: balances,
 		Sudo: sudo,
-		// Used for the module template in `./template.rs`
-		TemplateModule: template::{Module, Call, Storage, Event<T>},
+		Substratekitties: substratekitties::{Module, Call, Storage, Event<T>},
 	}
 );
 
