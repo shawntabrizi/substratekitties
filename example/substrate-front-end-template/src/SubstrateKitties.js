c15a3af08c31f4e32fdb43cb97c3d968e25058cf
Jimmy Chu <jimmychu0807@gmail.com>
2019-11-15 19:08:13 +0800

    get the kitty count and kitty properties

--- example/substrate-front-end-template/src/SubstrateKitties.js
+++ example/substrate-front-end-template/src/SubstrateKitties.js
@@ -15,21 +15,28 @@ export default function SubstrateKitties(props) {
   const [allKitties, setAllKitties] = useState([]);
 
   useEffect(() => {
-    let unsubscribe;
+    let unsubKittyCnt = null, unsubKitties = null;
+    const fetchKitties = async () => {
+      unsubKittyCnt = await api.query.substratekitties.kittiesCount(async kittyCnt => {
+        kittyCnt = kittyCnt.toNumber();
+        setKittiesCount(kittyCnt);
 
-    api.query.substratekitties
-      .kittiesCount(count => {
-        setKittiesCount(count);
-      })
-      .then(unsub => {
-        unsubscribe = unsub;
+        // Retrieve all kitties
+        unsubKitties = await api.query.substratekitties.kitties.multi(
+          Array.from(Array(kittyCnt).keys()), kitties => {
+          kitties = kitties.map(val => val.unwrapOr(null));
+          setAllKitties(kitties);
+        });
       });
+    }
 
-    api.query.substratekitties.kitties(kitties => {
-      console.log("Kitties", kitties);
-    });
+    fetchKitties();
 
-    return () => unsubscribe && unsubscribe();
+    return () => {
+      // clean up function
+      unsubKittyCnt && unsubKittyCnt();
+      unsubKitties && unsubKitties();
+    }
   }, [kittiesCount, api.query.substratekitties]);
 
   return (
